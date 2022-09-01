6d9a80f19747683c2e11be669339f4bbc1739d59
AneeshGuptaPHS <112594552+AneeshGuptaPHS@users.noreply.github.com>
2022-08-31 20:45:07 -0400

    added comments

--- Studies/WirbelJ.R
+++ Studies/WirbelJ.R
@@ -2,10 +2,15 @@ pacman::p_load("bugphyzz", "curatedMetagenomicData", "EnrichmentBrowser", "Summa
 source("old_make_signatures.R")
 source("deAnaPhyzz.R")
 
+#Opens the data
 ye <- curatedMetagenomicData("WirbelJ_2018.relative_abundance", dryrun = F, counts = T, rownames = "long")
 ye.se <- ye[[1]]
+
+#Makes the group which is needed to do deAna
 grp1 = ifelse(ye.se$study_condition == "control" , 0, 1)
 ye.se$GROUP = grp1
+
+#Differential gene expression analysis
 ye.se.Ana <- deAna(ye.se, de.method = "DESeq2", filter.by.expr = F)
 
 
@@ -22,7 +27,7 @@ reg_name <- function(MetaPhlAn){
   spl[length(spl)]
 }
 
-
+#changes the rownames to the species name
 rd <- rowData(ye.se.Ana)
 rownames(rd) <- vapply(rownames(rd), reg_name,
                           character(1), USE.NAMES = FALSE)
@@ -30,17 +35,16 @@ rownames(rd) <- vapply(rownames(rd), .getLast,
                           character(1), USE.NAMES = FALSE)
 rownames(rd) <- substr(rownames(rd), 4, nchar(rownames(rd)))
 
-
+#filters volcanoData to only contain the bacteria of interest
 sphingoProducers <- scan("new sps.txt", character(), quote = "")
 rd <- as.data.frame(rd)
 rd <- filter(rd, genus %in% sphingoProducers, abs(FC) != 30, FC != 0)
 rd[ order((rownames(rd))), ] 
 
-#test <- filter()
-
-#rownames(volcanoData) <- NULL
+#table of significant values
 volcanoDataRed <- filter(rd, log2(abs(FC))>1, ADJ.PVAL < .05)
 
+#Makes the volcano plot
 png(filename = "volcanoWirbelJ.png", res = 300, height = 1800, width = 2500)
 EnhancedVolcano(rd,
                 lab = rownames(rd),
